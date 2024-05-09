e7b9c1e1c1e31e304354cf6647eda096904122ce
sirse <artiom.tretjakovas2@gmail.com>
2024-05-09 16:06:38 +0300

    Fix linking issues

--- 381_xyzz_bal/sppark/rust/publish.sh
+++ 381_xyzz_bal/sppark/rust/publish.sh
@@ -3,9 +3,9 @@
 HERE=`dirname $0`
 cd "${HERE}"
 
-if [ ! -d sppark ]; then
-    trap '[ -h sppark ] && rm -f sppark' 0 2
-    ln -s .. sppark
+if [ ! -d sppark_bal ]; then
+    trap '[ -h sppark_bal ] && rm -f sppark_bal' 0 2
+    ln -s .. sppark_bal
 fi
 
 # --allow-dirty because the temporary sppark symbolic link is not committed
